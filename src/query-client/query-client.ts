901fb3b58d2e7da61761eb10ed60d5ffd5135764
Vladislav Guleaev <Vladislav.Guleaev@check24.de>
2024-07-17 21:09:41 +0200

    Disable React Query refetch on window focus

--- src/query-client/query-client.ts
+++ src/query-client/query-client.ts
@@ -2,4 +2,11 @@ import { QueryClient } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
+queryClient.setDefaultOptions({
+  queries: {
+    refetchOnWindowFocus: false,
+    retry: false,
+  },
+});
+
 export default queryClient;
